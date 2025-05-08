0753936ddefe88b571aac800d176bffd9e42596d
demola malomo <malomo.alaba@yahoo.com>
2025-05-08 01:44:47 +0100

    Logo path fix

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Logo from './Logo';
+import Logo from './logo';
 import { GitHubIcon } from 'nextra/icons';
 import { Link } from 'nextra-theme-docs';
 
 export default function Header() {
 	return (
-		<nav className='flex justify-between items-center border-b border-[#E5EBF0] px-5 lg:px-[4rem] py-3 lg:py-4'>
+		<nav className='flex justify-between items-center border-b border-[#E5EBF0] px-5 lg:px-[2.2rem] py-3 lg:py-4'>
 			<div className=''>
 				<Logo />
 			</div>
