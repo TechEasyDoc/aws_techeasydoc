b940b7399017ed1a623a7171f3349ef264279c67
demola malomo <malomo.alaba@yahoo.com>
2026-01-10 18:20:15 +0100

    adding google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,16 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Script
-				async
 				src='https://www.googletagmanager.com/gtag/js?id=G-JR1BBZFR1M'
-			></Script>
-			<Script>
-				{`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-JR1BBZFR1M');`}
+				strategy='afterInteractive'
+			/>
+			<Script id='google-analytics' strategy='afterInteractive'>
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-JR1BBZFR1M');
+				`}
 			</Script>
 			<Component {...pageProps} />
 		</>
